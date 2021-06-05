d4bd1b3289822219363d443e2f9e2732e8bca5df
JQ_Chan <jinqiu.chen@casstime.com>
2021-06-05 10:57:45 +0800

    use Head

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
@@ -11,6 +16,12 @@ export default function FirstPost() {
       </h2>
 
       <img src="/images/profile.jpg" alt="静态图片" />
+      <Image 
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="静态图片"
+      />
     </>
   )
 }
