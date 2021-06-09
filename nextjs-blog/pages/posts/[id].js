3b926f3bc48ba0fecbd355c4eda0f00743b0493c
JQ_Chan <jinqiu.chen@casstime.com>
2021-06-09 17:23:08 +0800

    Polishing the Post Page

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,18 +1,24 @@
+import Head from "next/head";
 import Layout from "../../components/layout/layout";
+import Date from "../../components/date";
+
+import utilStyles from "../../styles/utils.module.scss";
+
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title || ""}
-      <br />
-      {postData.id || ""}
-      <br />
-      {postData.date || ""}
-      <br />
-      <div
-        dangerouslySetInnerHTML={{ __html: postData.contentHtml || "" }}
-      ></div>
+      <Head>
+        <title>{postData.title || ''}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title || ''}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date || ''} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml || '' }} />
+      </article>
     </Layout>
   );
 }
